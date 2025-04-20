dd559d4607819cd787a1b443b309ee7a94efdb7b
CaretParrot <wopow29@gmail.com>
2025-04-20 17:50:31 -0500

    Make quiz title editable and improve question handling logic

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ id.setupTree();
 let answers = document.getElementsByClassName("answer");
 let quizzes = {};
 
-function addQuestion() {
-    let answerList = id.answers.value.split("\n");
-    answerList[id.correctAnswer.value - 1] += "☑";
-
-    quizzes[id.quizTitle.value].push(`${id.question.value}: ${answerList}`);
-    console.log(quizzes[id.quizTitle.value]);
-    id.questionList.innerHTML = "";
-
-    updateList(answerList);
-}
-
 id.createQuiz.onclick = function () {
     if (id.quizTitle.value) {
         pages.changePage('createPage');
         id.titleHeading.innerHTML = id.quizTitle.value;
-        quizzes[id.quizTitle.value] = [];
+        quizzes[id.quizTitle.value] = {
+            questions: []
+        };
     }
+}
+
+function addQuestion() {
+    let answerList = id.answers.value.split("\n");
+    answerList[id.correctAnswer.value - 1] += "☑";
+    quizzes[id.quizTitle.value].questions.push(`${id.question.value}: ${answerList}`);
+    console.log(quizzes[id.quizTitle.value]);
+
+    updateList(answerList);
 }
 
 id.quizTitle.onkeydown = function (event) {
@@ -32,13 +32,17 @@ id.quizTitle.onkeydown = function (event) {
 }
 
 function updateList(answerList) {
-    for (let i = 0; i < quizzes[id.quizTitle.value].length; i++) {
+    id.questionList.innerHTML = "";
+    for (let i = 0; i < quizzes[id.quizTitle.value].questions.length; i++) {
         let newQuestion = document.createElement("button");
         newQuestion.onclick = function () {
             this.remove();
-            quizzes[id.quizTitle.value].splice(quizzes[id.quizTitle.value].indexOf(this.textContent), 1);
+            quizzes[id.quizTitle.value].questions.splice(quizzes[id.quizTitle.value].questions.indexOf(this.textContent), 1);
+            console.log(quizzes[id.quizTitle.value]);
         }
-        newQuestion.innerHTML = quizzes[id.quizTitle.value][i];
+        newQuestion.innerHTML = quizzes[id.quizTitle.value].questions[i];
         id.questionList.appendChild(newQuestion);
     }
+
+    localStorage.setItem("quizzes", JSON.stringify(quizzes))
 }
